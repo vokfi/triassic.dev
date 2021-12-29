84f19d7b39a9a002dedff442a864179e5882efc1
RealTriassic <techbyteofficial9@gmail.com>
2021-12-29 19:16:53 +0200

    Final Coming Soon page improvement.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,18 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faPaintBrush } from '@fortawesome/free-solid-svg-icons'
+import { faCookieBite } from '@fortawesome/free-solid-svg-icons'
 
 export default function IndexPage() {
   return (
-  <>
-    <body className="bg-slate-900 h-screen">
-      <main>
+    <>
+      <div className="bg-slate-900 w-screen h-screen">
         <div className="grid place-items-center h-screen">
           <p className="font-bold text-slate-600 text-8xl">
-            <FontAwesomeIcon icon={faPaintBrush} size="1x" />
+            <FontAwesomeIcon icon={faCookieBite} size="1x" spin />
           </p>
-          <p className="font-bold text-slate-300 text-2xl">Triassic is currently painting the website...</p>
+          <p className="font-bold text-slate-300 text-2xl">The website is coming soon, I am working on it!</p>
+          <p className="italic text-slate-500 text-md">~ Meanwhile, enjoy the endless spinning cookie.</p>
         </div>
-      </main>
-    </body>
-  </>
+      </div>
+    </>
   )
 }
