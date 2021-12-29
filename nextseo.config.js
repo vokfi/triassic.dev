347e6f553c4274413121ff1d3282e9ff00b315e4
RealTriassic <techbyteofficial9@gmail.com>
2021-12-29 18:08:50 +0200

    SEO Stuff

---
+++ nextseo.config.js
@@ -0,0 +1,62 @@
+export default {
+    titleTemplate: '%s | Triassic',
+    defaultTitle: 'Triassic',
+    description: 'Hello, I am Daniel, also known as Triassic, I am a front-end web developer and designer and this website is all about me!',
+    canonical: 'https://triassic.dev',
+    openGraph: {
+        url: 'https://triassic.dev/',
+        type: 'website',
+        locale: 'en_US',
+        title: 'Triassic',
+        description: 'Hello, I am Daniel, also known as Triassic, I am a front-end web developer and designer and this website is all about me!',
+        site_name: "Triassic's Website",
+        images: [
+            {
+                url: "/favicons/favicon-196x196.ico",
+                width: 196,
+                height: 196,
+                alt: "Favicon 196x196",
+            },
+            {
+                url: "/favicons/favicon-180x180.ico",
+                width: 180,
+                height: 180,
+                alt: "Favicon 180x180",
+            },
+            {
+                url: "/favicons/favicon-128x128.ico",
+                width: 128,
+                height: 128,
+                alt: "Favicon 128x128",
+            },
+            {
+                url: "/favicons/favicon-64x64.ico",
+                width: 64,
+                height: 64,
+                alt: "Favicon 64x64",
+            },
+            {
+                url: "/favicons/favicon.ico",
+                width: 48,
+                height: 48,
+                alt: "Favicon 48x48",
+            },
+            {
+                url: "/favicons/favicon-32x32.ico",
+                width: 32,
+                height: 32,
+                alt: "Favicon 32x32",
+            },
+            {
+                url: "/favicons/favicon-16x16.ico",
+                width: 16,
+                height: 16,
+                alt: "Favicon 16x16",
+            },
+        ]
+    },
+    twitter: {
+        handle: '@RealTriassic',
+        cardType: 'summary',
+    },
+};
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,23 +1,11 @@
+import { DefaultSeo } from 'next-seo';
+import SEO from '../nextseo.config.js';
 import '../styles/style.css'
-import Head from 'next/head'
 
 function App({ Component, pageProps }) {
   return (
    <>
-    <Head>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-      <meta property="og:site_name" content="Triassic's Website" />
-      <meta property="og:title" content="Triassic" />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://triassic.dev" />
-      <meta property="og:image" content="/assets/img/triassic.png" />
-      <meta property="og:description" content="Hello, I am Daniel, also known as Triassic, I am a front-end web developer and designer and this website is all about me!" />
-      <meta name="description" content="Hello, I am Daniel, also known as Triassic, I am a front-end web developer and designer and this website is all about me!" />
-      <meta name="theme-color" content="#74b3fa" />
-
-      <title>Triassic</title>
-    </Head>
+    <DefaultSeo {...SEO} />
     <Component {...pageProps} />
    </>
   )
